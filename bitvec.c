82a5ff7d5727289d07736614ba91f34764bf9d1b
fredrikhederstierna <fredrik@hederstierna.com>
2021-04-19 07:55:23 +0200

    added comments

--- bitvec.c
+++ bitvec.c
@@ -16,6 +16,7 @@
 static uint8_t *bitvec_data          = NULL;
 static uint32_t bitvec_encoded_nbits = 0;
 
+// TODO: optimize, this is unnecessary slow, use local registers
 void bitvec_insert_bits(uint8_t *bv, uint32_t nbits)
 {
   uint32_t bi;
@@ -24,11 +25,13 @@ void bitvec_insert_bits(uint8_t *bv, uint32_t nbits)
     bitvec_data[bitvec_encoded_nbits >> 3] |= (newbit << (bitvec_encoded_nbits & 7));
     bitvec_encoded_nbits++;
     if ((bitvec_encoded_nbits & 7) == 0) {
+      // clear next byte to be inserted into bitvector
       bitvec_data[bitvec_encoded_nbits >> 3] = 0;
     }
   }
 }
 
+// TODO: optimize, this is unnecessary slow, use local registers
 void bitvec_extract_bits(uint8_t *bv, uint32_t nbits)
 {
   int32_t bitvec_cur_pos = bitvec_encoded_nbits - nbits;
@@ -39,15 +42,15 @@ void bitvec_extract_bits(uint8_t *bv, uint32_t nbits)
     uint8_t curbit = (bitvec_data[bitvec_cur_pos >> 3] & (1 << (bitvec_cur_pos & 7))) ? 1 : 0;
     next_byte |= (curbit << (bi & 7));
     if ((bi & 7) == 7) {
-      // store next byte
+      // store next extracted byte
       bv[bv_wr_pos++] = next_byte;
       next_byte = 0;
     }
     bitvec_cur_pos++;
     bitvec_encoded_nbits--;
   }
-  // store last byte
   if ((nbits & 7) > 0) {
+    // store last extracted byte if uneven byte boundary
     bv[bv_wr_pos++] = next_byte;
   }
 }
@@ -72,6 +75,9 @@ uint32_t bitvec_get_encoded_nbits(void)
 void bitvec_init(uint8_t *encoded_data,
                  uint32_t encoded_nbits)
 {
+  // Setup the encoded bit vector and length,
+  // normally nbits is zero when staring encoding,
+  // and if decoding nbits is full encoded length.
   bitvec_data          = encoded_data;
   bitvec_encoded_nbits = encoded_nbits;
 }
